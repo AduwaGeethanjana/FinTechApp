6c4f583b98139dd539e43ac2db8415de0d15aaac
AduwaGeethanjana <sarageethanjana@gmail.com>
2025-01-29 18:10:05 +0530

    Finalize the app

--- components/ui/BankCard.tsx
+++ components/ui/BankCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
+import Copy from './Copy'
 
 const BankCard = ({account,userName,showBalance= true}: CreditCardProps ) => {
   return (
@@ -10,7 +11,7 @@ const BankCard = ({account,userName,showBalance= true}: CreditCardProps ) => {
         <div className="bank-card_content">
         <div>
             <h1 className="text-16 font-semibold text-white">
-                {userName}
+                {account.name}
             </h1>
             <p className="font-ibm-plex-serif font-black text-white">
                 {formatAmount(account.currentBalance)}
@@ -59,6 +60,7 @@ const BankCard = ({account,userName,showBalance= true}: CreditCardProps ) => {
           className="absolute top-0 left-0"
         />
         </Link>
+        {showBalance && <Copy title={account?.sharableId}/>}
     </div>
   )
 }
--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -6,6 +6,7 @@ import { sidebarLinks } from '@/constants';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import Footer from './Footer';
+import PlaidLink from './PlaidLink';
 
 
 const Sidebar = ({ user }: SiderbarProps) => {
@@ -50,7 +51,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
             );
           })}
 
-          USER
+          <PlaidLink user={user} />
         </nav>
         <Footer user={user}/>
       </section>
